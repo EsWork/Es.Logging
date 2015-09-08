d568fc6619b81a63949eff6ad6ff0357c4d3ae63
v.la <v.la@live.cn>
2015-09-08 21:53:21 +0800

    Added NLog Extension

---
+++ src/Es.Logging.NLog/NLogLoggerFactoryExtensions.cs
@@ -0,0 +1,12 @@
+﻿using NLog;
+
+namespace Es.Logging
+{
+    public static class NLogLoggerFactoryExtensions
+    {
+        public static ILoggerFactory AddNLog(this ILoggerFactory factory, LogFactory logFactory) {
+            factory.AddProvider(new NLogLoggerProvider(logFactory));
+            return factory;
+        }
+    }
+}
