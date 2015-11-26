932fd35fc6cf636cb7bf754878d5616a3e23cffe
v.la <v.la@live.cn>
2015-11-26 19:26:45 +0800

    upgrade DLL and assembly signature

--- nuget-push.cmd
+++ nuget-push.cmd
@@ -1,5 +1,5 @@
 set ng="%~dp0Tools\NuGet\NuGet.exe"
-%ng% push %~dp0artifacts\Es.Logging.0.0.2.nupkg
-%ng% push %~dp0artifacts\Es.Logging.Console.0.0.2.nupkg
-%ng% push %~dp0artifacts\Es.Logging.Log4.0.0.2.nupkg
-%ng% push %~dp0artifacts\Es.Logging.NLog.0.0.2.nupkg
+%ng% push %~dp0artifacts\Es.Logging.0.0.3.nupkg
+%ng% push %~dp0artifacts\Es.Logging.Console.0.0.3.nupkg
+%ng% push %~dp0artifacts\Es.Logging.Log4.0.0.3.nupkg
+%ng% push %~dp0artifacts\Es.Logging.NLog.0.0.3.nupkg
