1681e44d00377439f193556ae2f006e7fad34e8e
v.la <v.la@live.cn>
2016-11-03 14:08:27 +0800

    modify build output to build-base-path

--- build.cmd
+++ build.cmd
@@ -5,15 +5,15 @@ call dotnet restore src/Es.Logging.Console
 call dotnet restore src/Es.Logging.Log4
 call dotnet restore src/Es.Logging.NLog
 
-call dotnet build -f netstandard1.6 -c Release src/Es.Logging -o artifacts/netstandard1.6 
-call dotnet build -f netstandard1.6 -c Release src/Es.Logging.Console -o artifacts/netstandard1.6 
-call dotnet build -f netstandard1.6 -c Release src/Es.Logging.Log4 -o artifacts/netstandard1.6 
-call dotnet build -f netstandard1.6 -c Release src/Es.Logging.NLog -o artifacts/netstandard1.6 
+call dotnet build -f netstandard1.6 -c Release src/Es.Logging -b artifacts
+call dotnet build -f netstandard1.6 -c Release src/Es.Logging.Console -b artifacts
+call dotnet build -f netstandard1.6 -c Release src/Es.Logging.Log4 -b artifacts
+call dotnet build -f netstandard1.6 -c Release src/Es.Logging.NLog -b artifacts
 
-call dotnet build -f net45 -c Release src/Es.Logging -o artifacts/net45
-call dotnet build -f net45 -c Release src/Es.Logging.Console -o artifacts/net45
-call dotnet build -f net45 -c Release src/Es.Logging.Log4 -o artifacts/net45
-call dotnet build -f net45 -c Release src/Es.Logging.NLog -o artifacts/net45
+call dotnet build -f net45 -c Release src/Es.Logging -b artifacts
+call dotnet build -f net45 -c Release src/Es.Logging.Console -b artifacts
+call dotnet build -f net45 -c Release src/Es.Logging.Log4 -b artifacts
+call dotnet build -f net45 -c Release src/Es.Logging.NLog -b artifacts
 
 call dotnet pack --configuration release src/Es.Logging  -o artifacts
 call dotnet pack --configuration release src/Es.Logging.Console  -o artifacts
