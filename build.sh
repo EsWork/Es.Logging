90e5e9d73234e42e4852bf1d613793e4b4ab8595
v.la <v.la@live.cn>
2016-12-25 23:59:58 +0800

    删除dotnet安装脚本

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 
-sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
-apt-get update -y
-apt-get install dotnet-dev-1.0.0-preview2.1-003177 -y
-
 dotnet restore src/Es.Logging
 dotnet restore src/Es.Logging.Console
 dotnet restore src/Es.Logging.Log4
