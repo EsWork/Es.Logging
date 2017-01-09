6276a288b2aa310f8659cbe09e969274328d2559
v.la <v.la@live.cn>
2017-01-09 13:44:41 +0800

    added NLogXmlConfigure usage

---
+++ src/Sample/NLogXmlConfigureDemo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Es.Logging;
+using NLog;
+
+namespace Sample
+{
+    public class NLogXmlConfigureDemo
+    {
+        private readonly ILoggerFactory _logFactory;
+
+        public NLogXmlConfigureDemo() {
+            _logFactory = new LoggerFactory();
+
+            string filename = "";
+#if NETFULL
+            filename = AppDomain.CurrentDomain.BaseDirectory + "NLog.xml";
+#else
+            filename = AppContext.BaseDirectory + "/NLog.xml";
+#endif
+            _logFactory.AddNLog(filename);
+        }
+
+        [Demo]
+        public void WriteLog() {
+
+            var log = _logFactory.CreateLogger("ConsoleDemo");
+
+            log.Trace("Trace....");
+            log.Debug("Verbose...");
+            log.Info("Information....");
+            log.Error("Error...");
+            log.Warn("Warning...");
+            log.Fatal("Fatal...");
+
+            var exception = new InvalidOperationException("Invalid value");
+
+            log.Error(exception);
+        }
+    }
+}
