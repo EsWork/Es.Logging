3f7b7fc56512c347bb52c8f1b4d74688c100daac
v.la <v.la@live.cn>
2017-03-25 12:20:26 +0800

    remove package build

--- build.sh
+++ build.sh
@@ -21,9 +21,3 @@ dotnet build src/Es.Logging.NLog -f netstandard1.3 -c Release -o ${artifacts}/ne
 dotnet build src/Es.Logging.Log4 -f netstandard1.3 -c Release -o ${artifacts}/netstandard1.3
 dotnet build src/Es.Microsoft.Log -f netstandard1.3 -c Release -o ${artifacts}/netstandard1.3
 
-dotnet pack src/Es.Logging -c release -o ${artifacts}
-dotnet pack src/Es.Logging.Console -c release -o ${artifacts}
-dotnet pack src/Es.Logging.Log4 -c release -o ${artifacts}
-dotnet pack src/Es.Logging.NLog -c release -o ${artifacts}
-dotnet pack src/Es.Microsoft.Log -c release -o ${artifacts}
-
