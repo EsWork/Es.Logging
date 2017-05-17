b0e3b690c33ee1050f4b767b7d35bcc2d6dd00b1
v.la <v.la@live.cn>
2017-05-17 17:27:16 +0800

    fix nuget_push.cmd

--- nuget_push.cmd
+++ nuget_push.cmd
@@ -1,5 +1,8 @@
 set /p ver=<VERSION
 set sourceUrl=-s https://www.nuget.org/api/v2/package
 
-
+dotnet nuget push artifacts/Es.Logging.%ver%.nupkg %sourceUrl%
+dotnet nuget push artifacts/Es.Logging.Console.%ver%.nupkg %sourceUrl%
+dotnet nuget push artifacts/Es.Logging.NLog.%ver%.nupkg %sourceUrl%
+dotnet nuget push artifacts/Es.Logging.Log4.%ver%.nupkg %sourceUrl%
 dotnet nuget push artifacts/Es.Microsoft.Log.%ver%.nupkg %sourceUrl%
