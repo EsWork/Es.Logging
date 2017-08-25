48aec2edfe120d4678be04665813804783362c4c
v.la <v.la@live.cn>
2017-08-25 15:22:16 +0800

    change version to 1.1.3-beta2

--- build.cmd
+++ build.cmd
@@ -16,6 +16,8 @@ call dotnet build src/Es.Logging.NLog -f netstandard1.3 -c Release -o %artifacts
 call dotnet build src/Es.Logging.NLog -f netstandard2.0 -c Release -o %artifacts%\netstandard2.0
 call dotnet build src/Es.Logging.Log4 -f netstandard1.3 -c Release -o %artifacts%\netstandard1.3
 call dotnet build src/Es.Logging.Log4 -f netstandard2.0 -c Release -o %artifacts%\netstandard2.0
+call dotnet build src/Es.Logging.Serilog -f netstandard1.3 -c Release -o %artifacts%\netstandard1.3
+call dotnet build src/Es.Logging.Serilog -f netstandard2.0 -c Release -o %artifacts%\netstandard2.0
 call dotnet build src/Es.Microsoft.Log -f netstandard1.3 -c Release -o %artifacts%\netstandard1.3
 call dotnet build src/Es.Microsoft.Log -f netstandard2.0 -c Release -o %artifacts%\netstandard2.0
 
@@ -28,4 +30,5 @@ call dotnet pack src/Es.Logging -c release -o %artifacts%
 call dotnet pack src/Es.Logging.Console -c release -o %artifacts%
 call dotnet pack src/Es.Logging.Log4 -c release -o %artifacts%
 call dotnet pack src/Es.Logging.NLog -c release -o %artifacts%
+call dotnet pack src/Es.Logging.Serilog -c release -o %artifacts%
 call dotnet pack src/Es.Microsoft.Log -c release -o %artifacts%
--- build.sh
+++ build.sh
@@ -23,6 +23,8 @@ dotnet build src/Es.Logging.NLog -f netstandard1.3 -c Release -o ${artifacts}/ne
 dotnet build src/Es.Logging.NLog -f netstandard2.0 -c Release -o ${artifacts}/netstandard2.0
 dotnet build src/Es.Logging.Log4 -f netstandard1.3 -c Release -o ${artifacts}/netstandard1.3
 dotnet build src/Es.Logging.Log4 -f netstandard2.0 -c Release -o ${artifacts}/netstandard2.0
+dotnet build src/Es.Logging.Serilog -f netstandard1.3 -c Release -o ${artifacts}/netstandard1.3
+dotnet build src/Es.Logging.Serilog -f netstandard2.0 -c Release -o ${artifacts}/netstandard2.0
 dotnet build src/Es.Microsoft.Log -f netstandard1.3 -c Release -o ${artifacts}/netstandard1.3
 dotnet build src/Es.Microsoft.Log -f netstandard2.0 -c Release -o ${artifacts}/netstandard2.0
 
