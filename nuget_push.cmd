8134f89f67585f1f5307ad78e05f52923b45acf7
v.la <v.la@live.cn>
2017-08-25 22:00:34 +0800

    增加Serilog发布到Nuget

--- nuget_push.cmd
+++ nuget_push.cmd
@@ -5,4 +5,5 @@ dotnet nuget push artifacts/Es.Logging.%ver%.nupkg %sourceUrl%
 dotnet nuget push artifacts/Es.Logging.Console.%ver%.nupkg %sourceUrl%
 dotnet nuget push artifacts/Es.Logging.NLog.%ver%.nupkg %sourceUrl%
 dotnet nuget push artifacts/Es.Logging.Log4.%ver%.nupkg %sourceUrl%
+dotnet nuget push artifacts/Es.Logging.Serilog.%ver%.nupkg %sourceUrl%
 dotnet nuget push artifacts/Es.Microsoft.Logging.%ver%.nupkg %sourceUrl%
