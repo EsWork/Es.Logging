36f7652ab84398c03f4a16da094fe87757f2dd80
v.la <v.la@live.cn>
2018-11-27 14:05:41 +0800

    chore: change version to 1.1.11

--- build.cmd
+++ build.cmd
@@ -8,23 +8,23 @@ call dotnet restore src/Es.Logging.Log4
 call dotnet restore src/Es.Logging.NLog
 call dotnet restore src/Es.Microsoft.Log
 
-call dotnet build src/Es.Logging -f netstandard1.3 -c Release -o %artifacts%\netstandard1.3
-call dotnet build src/Es.Logging -f netstandard2.0 -c Release -o %artifacts%\netstandard2.0
-call dotnet build src/Es.Logging.Console -f netstandard1.3 -c Release -o %artifacts%\netstandard1.3
-call dotnet build src/Es.Logging.Console -f netstandard2.0 -c Release -o %artifacts%\netstandard2.0
-call dotnet build src/Es.Logging.NLog -f netstandard2.0 -c Release -o %artifacts%\netstandard2.0
-call dotnet build src/Es.Logging.Log4 -f netstandard1.3 -c Release -o %artifacts%\netstandard1.3
-call dotnet build src/Es.Logging.Log4 -f netstandard2.0 -c Release -o %artifacts%\netstandard2.0
-call dotnet build src/Es.Logging.Serilog -f netstandard1.3 -c Release -o %artifacts%\netstandard1.3
-call dotnet build src/Es.Logging.Serilog -f netstandard2.0 -c Release -o %artifacts%\netstandard2.0
-call dotnet build src/Es.Microsoft.Log -f netstandard1.3 -c Release -o %artifacts%\netstandard1.3
-call dotnet build src/Es.Microsoft.Log -f netstandard2.0 -c Release -o %artifacts%\netstandard2.0
+REM call dotnet build src/Es.Logging -f netstandard1.3 -c Release -o %artifacts%\netstandard1.3
+REM call dotnet build src/Es.Logging -f netstandard2.0 -c Release -o %artifacts%\netstandard2.0
+REM call dotnet build src/Es.Logging.Console -f netstandard1.3 -c Release -o %artifacts%\netstandard1.3
+REM call dotnet build src/Es.Logging.Console -f netstandard2.0 -c Release -o %artifacts%\netstandard2.0
+REM call dotnet build src/Es.Logging.NLog -f netstandard2.0 -c Release -o %artifacts%\netstandard2.0
+REM call dotnet build src/Es.Logging.Log4 -f netstandard1.3 -c Release -o %artifacts%\netstandard1.3
+REM call dotnet build src/Es.Logging.Log4 -f netstandard2.0 -c Release -o %artifacts%\netstandard2.0
+REM call dotnet build src/Es.Logging.Serilog -f netstandard1.3 -c Release -o %artifacts%\netstandard1.3
+REM call dotnet build src/Es.Logging.Serilog -f netstandard2.0 -c Release -o %artifacts%\netstandard2.0
+REM call dotnet build src/Es.Microsoft.Log -f netstandard1.3 -c Release -o %artifacts%\netstandard1.3
+REM call dotnet build src/Es.Microsoft.Log -f netstandard2.0 -c Release -o %artifacts%\netstandard2.0
 
-call dotnet build src/Es.Logging -f net45 -c Release -o %artifacts%\net45
-call dotnet build src/Es.Logging.Console -f net45 -c Release -o %artifacts%\net45
-call dotnet build src/Es.Logging.NLog -f net45 -c Release -o %artifacts%\net45
-call dotnet build src/Es.Logging.Log4 -f net45 -c Release -o %artifacts%\net45
-call dotnet build src/Es.Logging.Serilog -f net45 -c Release -o %artifacts%\net45
+REM call dotnet build src/Es.Logging -f net45 -c Release -o %artifacts%\net45
+REM call dotnet build src/Es.Logging.Console -f net45 -c Release -o %artifacts%\net45
+REM call dotnet build src/Es.Logging.NLog -f net45 -c Release -o %artifacts%\net45
+REM call dotnet build src/Es.Logging.Log4 -f net45 -c Release -o %artifacts%\net45
+REM call dotnet build src/Es.Logging.Serilog -f net45 -c Release -o %artifacts%\net45
 
 call dotnet pack src/Es.Logging -c release -o %artifacts%
 call dotnet pack src/Es.Logging.Console -c release -o %artifacts%
@@ -32,3 +32,5 @@ call dotnet pack src/Es.Logging.Log4 -c release -o %artifacts%
 call dotnet pack src/Es.Logging.NLog -c release -o %artifacts%
 call dotnet pack src/Es.Logging.Serilog -c release -o %artifacts%
 call dotnet pack src/Es.Microsoft.Log -c release -o %artifacts%
+
+pause
