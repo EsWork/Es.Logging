ddc0a9d231440e9630097b2c0cadf104e498dd48
v.la <v.la@live.cn>
2022-03-05 00:29:29 +0800

    chore: change version to 1.2.3

--- build.cmd
+++ build.cmd
@@ -2,17 +2,19 @@ set artifacts=%~dp0artifacts
 
 if exist %artifacts%  rd /q /s %artifacts%
 
+set /p ver=<VERSION
+
 dotnet restore src/Es.Logging
 dotnet restore src/Es.Logging.Console
 dotnet restore src/Es.Logging.Log4
 dotnet restore src/Es.Logging.NLog
 dotnet restore src/Es.Microsoft.Log
 
-dotnet pack  -c release  src/Es.Logging -o %artifacts%
-dotnet pack  -c release  src/Es.Logging.Console -o %artifacts%
-dotnet pack  -c release  src/Es.Logging.Log4 -o %artifacts%
-dotnet pack  -c release  src/Es.Logging.NLog -o %artifacts%
-dotnet pack  -c release  src/Es.Logging.Serilog -o %artifacts%
-dotnet pack  -c release  src/Es.Microsoft.Log -o %artifacts%
+dotnet pack  -c release -p:Ver=%ver% src/Es.Logging -o %artifacts%
+dotnet pack  -c release -p:Ver=%ver% src/Es.Logging.Console -o %artifacts%
+dotnet pack  -c release -p:Ver=%ver% src/Es.Logging.Log4 -o %artifacts%
+dotnet pack  -c release -p:Ver=%ver% src/Es.Logging.NLog -o %artifacts%
+dotnet pack  -c release -p:Ver=%ver% src/Es.Logging.Serilog -o %artifacts%
+dotnet pack  -c release -p:Ver=%ver% src/Es.Microsoft.Log -o %artifacts%
 
 pause
